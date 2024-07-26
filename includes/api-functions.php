c2bea422cdf678d6f79fb7e72642bf79737ead4f
Tronciu Dumitru <72997097+Parsyfall@users.noreply.github.com>
2024-07-26 13:09:05 +0300

    Add description for repo (#1)
    * refactor: split plugin into 'modules'
    
    * chore: add readme
    * chore: add info in plugin header

---
+++ includes/api-functions.php
@@ -0,0 +1,74 @@
+<?php
+
+function add_weather_api_route()
+{
+    register_rest_route(
+        'myplugin/v1',
+        '/weather',
+        array(
+            'methods'  => WP_REST_Server::READABLE,
+            'callback' => 'MWW_get_weather_data'
+        )
+    );
+}
+
+function MWW_get_weather_data(WP_REST_Request $request)
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'mww_weather_records';
+    $last_entry = $wpdb->get_results("SELECT * from $table_name order by date_time desc limit 1;", OBJECT)[0];
+    $city = $request->get_query_params()['city'];
+
+    write_log("In function " . __FUNCTION__);
+    
+    $url = 'http://api.weatherapi.com/v1/current.json?key=' . WEATHER_API_KEY . '&q=' . $city . '&aqi=no';
+
+    $response = wp_remote_get($url, array('format' => 'JSON'));
+
+    write_log(wp_remote_retrieve_response_code($response));
+    $json = json_decode($response['body'], true);
+
+    if (wp_remote_retrieve_response_code($response) >= 400) {
+        write_log('Notify admin, there are problems');
+        $error_code = $json['error']['code'];
+        $error_msg = $json['error']['message'];
+        AdminNotice::displayError("An error occurred in '". plugin_basename(__FILE__) . "': $error_msg Error code: $error_code");
+
+        return new WP_REST_Response(array(), wp_remote_retrieve_response_code($response));
+    }
+
+    if (is_wp_error($response)) {
+        if (is_null($last_entry)) {
+            write_log('Error and no db data');
+            return new WP_Error(array(), 5004);
+        }
+
+        write_log('No response from weather api, lucky me I have some data in db');
+        $db_data = json_decode(json_encode($last_entry), true);
+        return new WP_REST_Response($db_data, 2001);
+    }
+
+    if (is_null($last_entry) || time() - $last_entry->date_time >= 1800) {
+        write_log('Update data in db');
+
+        $data = array(
+            'date_time'      => time(),
+            'city'           => $json['location']['name'],
+            'weather_status' => $json['current']['condition']['text'],
+            'temp_c'         => $json['current']['temp_c'],
+            'wind_speed'     => $json['current']['wind_kph'],
+            'wind_dir'       => $json['current']['wind_dir'],
+            'atm_press'      => round($json['current']['pressure_mb'] * 0.750062, 1),
+            'humidity'       => $json['current']['humidity'],
+        );
+
+        $format = array('%d', '%s', '%s', '%f', '%f', '%s', '%f', '%d');
+
+        $wpdb->insert($table_name, $data, $format);
+    }
+
+    $data = json_decode(json_encode($last_entry), true);
+    write_log('Render data');
+
+    return new WP_REST_Response($data, 200);
+}
---
+++ includes/class-my-weather-widget.php
@@ -0,0 +1,66 @@
+<?php
+
+class My_Weather_Widget extends WP_Widget
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'my_weather_widget',
+            'My_Weather_Widget',
+            array('description' => __('A widget to display weather in a city', 'text-domain'))
+        );
+
+        add_action('widgets_init', function () {
+            register_widget('My_Weather_Widget');
+        });
+        register_activation_hook(__FILE__, 'MWW_create_db');
+        register_uninstall_hook(__FILE__, 'MWW_uninstall');
+        add_action('plugins_loaded', 'MWW_update_db_check');
+        add_action('rest_api_init', 'add_weather_api_route');
+        add_action('admin_notices', [new AdminNotice(), 'displayAdminNotice']);
+    }
+
+    private function get_weather(string $city)
+    {
+        $response = wp_remote_get(
+            'http://localhost:10008/wp-json/myplugin/v1/weather',
+            array('body' => array('city' => $city))
+        );
+        $code = wp_remote_retrieve_response_code($response);
+        $body = wp_remote_retrieve_body($response);
+        write_log('In function ' . __FUNCTION__ . ' response code: ' . $code);
+        $ret_val = json_decode($body, true);
+        write_log('End of ' . __FUNCTION__);
+        return MWW_render_response($ret_val);
+    }
+
+    public function widget($args, $instance)
+    {
+        extract($args);
+        echo $before_widget;
+        $city = $instance['city'];
+        echo $this->get_weather($city);
+        write_log("In function " . __FUNCTION__ . ' done rendering');
+        echo $after_widget;
+    }
+
+    public function form($instance)
+    {
+        $city = isset($instance['city']) ? $instance['city'] : __('City', 'text-domain');
+        $field_id = $this->get_field_id('city');
+        $field_name = $this->get_field_name('city');
+        ?>
+        <p>
+            <label for="<?php echo $field_id ?>" id="<?php echo $field_name . 'label' ?>">City</label>
+            <input id="<?php echo $field_id ?>" name="<?php echo $field_name ?>" type="text" class="widefat" placeholder="eg. London" value="<?php echo esc_attr($city); ?>">
+        </p>
+        <?php
+    }
+
+    public function update($new_instance, $old_instance)
+    {
+        $instance = array();
+        $instance['city'] = (!empty($new_instance['city'])) ? strip_tags($new_instance['city']) : '';
+        return $instance;
+    }
+}

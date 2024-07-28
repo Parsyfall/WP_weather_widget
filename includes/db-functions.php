c4b684c6c323a5cd578df062e7af04744f8daa66
Parsyfall <dumitru.tronciu@gmail.com>
2024-07-28 16:18:56 +0300

    feat: remove check for table existance

--- includes/db-functions.php
+++ includes/db-functions.php
@@ -10,7 +10,7 @@ function MWW_create_db()
     $table_name = $wpdb->prefix . 'mww_weather_records';
     $charset_collate = $wpdb->get_charset_collate();
 
-    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+    $sql = "CREATE TABLE $table_name (
         date_time int(11) PRIMARY KEY not NULL,
         city varchar(20) NOT NULL,
         weather_status text default '',

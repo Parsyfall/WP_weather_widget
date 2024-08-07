14c4b7e88c66c89380d1802f214e92bfbf2a6f47
Parsyfall <dumitru.tronciu@gmail.com>
2024-08-07 16:35:23 +0300

    feat: move db_functions to a class 	- Delegate creation, updating and deletion of db table to class 	  DataBaseManipulator 	- Modify hooks to correctly use new class

---
+++ includes/DataBaseManipulator.php
@@ -0,0 +1,79 @@
+<?php
+
+// TODO: Incapsulate all these functions in a class
+
+
+namespace MyWeatherWidget;
+
+class DataBaseManipulator
+{
+    private static $table_name;
+    private static $version;
+    private static $instance;
+
+    private function __construct()
+    {
+        self::$version = '0.1';
+        if (get_site_option('MWW_db_version') !== self::$version) {
+            $this->updateTable();
+        }
+
+        $this->createDbTable();
+        // TODO: Move install and unistall hooks here
+    }
+
+    public static function getInstance(): DataBaseManipulator
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new DataBaseManipulator();
+        }
+
+        return self::$instance;
+    }
+
+    private function createDbTable(): void
+    {
+        global $wpdb;
+
+        self::$table_name = $wpdb->prefix . 'mww_weather_records';
+        $charset_collate = $wpdb->get_charset_collate();
+
+        // TODO: Test if table is created correctly
+        $sql = "CREATE TABLE " . self::$table_name . " (
+            date_time int(11) PRIMARY KEY not NULL,
+            city varchar(20) NOT NULL,
+            weather_status text default '',
+            temp_c decimal not NULL,
+            wind_speed decimal not NULL,
+            wind_dir char(3) NOT null,
+            atm_press decimal not NULL,
+            humidity int not null 
+        ) $charset_collate;";
+
+        $wpdb->query($sql);
+
+        add_option('MWW_db_version', self::$version);
+    }
+
+    public function dropTable(): void
+    {
+        global $wpdb;
+        $sql = "DROP TABLE " . self::$table_name . ";";
+        $wpdb->query($sql);
+
+        delete_option('MWW_db_version');
+    }
+
+    public function updateTable(): void
+    { // Reserved for future db structural modifications / migrations
+        // FIXME: Not entirely implemented, provide a implementation
+
+        // Update version number
+        self::$version = '';
+
+        $sql = '';
+        global $wpdb;
+        $wpdb->query($sql);
+        update_option('MWW_db_version', self::$version);
+    }
+}
--- my_weather_widget.php
+++ my_weather_widget.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: My Weather Widget
  * Description: Displays current weather for a specified city using data from WeatherAPI.com. Easily add this widget to your sidebar or other widget areas to show live weather updates. The widget can be customized to display various weather details such as temperature, wind speed, humidity, and atmospheric pressure. Updates weather data every 30 minutes to ensure it provides the latest information.
@@ -13,8 +14,6 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-use MyWeatherWidget\AdminNotice;
-use MyWeatherWidget\My_Weather_Widget;
 
 // Load dependencies
 require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
@@ -29,17 +28,18 @@ $dotenv = \Dotenv\Dotenv::createImmutable(plugin_dir_path(__FILE__));
 $dotenv->load();
 define('WEATHER_API_KEY', $_ENV['WEATHER_API_KEY']);
 
+$dbManipulator = MyWeatherWidget\DataBaseManipulator::getInstance();
 
 // Register hooks
-register_activation_hook(__FILE__, 'MyWeatherWidget\MWW_create_db');
-register_uninstall_hook(__FILE__, 'MyWeatherWidget\MWW_uninstall');
-add_action('plugins_loaded', 'MyWeatherWidget\MWW_update_db_check');
+register_activation_hook(__FILE__, [$dbManipulator, 'createDbTable']);
+register_uninstall_hook(__FILE__, [$dbManipulator, 'dropTable']);
+add_action('plugins_loaded', [$dbManipulator, 'updateTable']);
 add_action('rest_api_init', 'MyWeatherWidget\MWW_add_weather_api_route');
 add_action('widgets_init', function () {
     register_widget('MyWeatherWidget\\My_Weather_Widget');
 });
-add_action('admin_notices', [new AdminNotice(), 'displayAdminNotice']);
+add_action('admin_notices', [new MyWeatherWidget\AdminNotice(), 'displayAdminNotice']);
 
 
 // Initialize widget
-$widget = new My_Weather_Widget();
+$widget = new MyWeatherWidget\My_Weather_Widget();
